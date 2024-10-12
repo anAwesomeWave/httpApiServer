91b7b17e7afcd0c0cd37b7405743d6d19fcb0b59
Timofey Zubov <timofey.zubov.05@gmail.com>
2024-10-12 14:06:47 +0300

    Add logger

--- cmd/urlShortener/main.go
+++ cmd/urlShortener/main.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"urlShortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -11,8 +19,31 @@ func main() {
 
 	fmt.Println(cfg)
 	// logger -
-
+	log := setupLogger(cfg.Env)
+	log.Info("Logger created.", slog.String("env", cfg.Env))
+	log.Debug("Starting url-shortener")
 	// storage - sqlite
 
 	// router - chi, render
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		panic(fmt.Sprintf("Wrong Env value {%v}", env))
+	}
+	return log
 }
